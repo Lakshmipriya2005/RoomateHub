7ea8bbdb59a8f7212524b60bc741795bd74a94ec
Lakshmipriya2005 <lakshmipriya2725@gmail.com>
2025-09-07 19:16:27 +0530

    Commit message

---
+++ roomatehub/src/test/java/com/rommieshub/roomatehub/RoomatehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rommieshub.roomatehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomatehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
